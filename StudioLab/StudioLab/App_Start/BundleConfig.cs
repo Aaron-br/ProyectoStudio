cbabe8ffb02580cdbacd3c5d18552af1369d787b
Aaron <aaronbellotrovira@gmail.com>
2017-02-08 23:10:58 +0100

    Añadidos elementos para hacer draggable - Jquery

--- StudioLab/StudioLab/App_Start/BundleConfig.cs
+++ StudioLab/StudioLab/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace StudioLab
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/MisEstilosGeneral.css"));
             bundles.Add(new StyleBundle("~/Content/cssjqueryUI").Include(
                      "~/Content/themes/base/*.css"));
         }
