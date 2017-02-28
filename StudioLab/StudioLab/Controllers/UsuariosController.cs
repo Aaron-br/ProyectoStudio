52bfea6c1ad0a9820ac04b937d81d023c8dbe6ff
Aaron <aaronbellotrovira@gmail.com>
2017-03-01 00:33:45 +0100

    Añadido listado de operaciones CRUD

---
+++ StudioLab/StudioLab/Controllers/UsuariosController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using StudioLab.Models.ViewModels;
+using StudioLab.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudioLab.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+
+        //Objeto que nos pertmite acceder a la BD mapaeada.
+        private StudioLabDBEntities myDB;
+
+
+        public ActionResult Listado()
+        {
+
+            //Definimos el viewModel creado para los usuarios
+            //Y lo rellenamos de datos de la BD.
+            UsuariosViewModel vm;
+            vm = new UsuariosViewModel();
+
+            myDB = new StudioLabDBEntities();
+
+            //Asignamos al viewModel la lista de usuario proveniente de la BD
+            vm.usuarios = myDB.Usuarios.ToList();
+
+
+            return View(vm);
+        }
+    }
+}
---
+++ StudioLab/StudioLab/Models/ViewModels/UsuariosViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudioLab.Models.ViewModels
+{
+    public class UsuariosViewModel
+    {
+
+        //Definimos una lista de Usuarios de la BD
+        public List <Usuarios> usuarios { get; set; }
+
+    }
+}
