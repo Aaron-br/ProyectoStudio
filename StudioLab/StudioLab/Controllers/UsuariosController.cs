bd1c6487784a787d13416b24d5c57553ccf2f729
Aaron <aaronbellotrovira@gmail.com>
2017-03-01 11:18:07 +0100

    Añadido Borrado de usuarios en CRUD

--- StudioLab/StudioLab/Controllers/UsuariosController.cs
+++ StudioLab/StudioLab/Controllers/UsuariosController.cs
@@ -12,6 +12,7 @@ namespace StudioLab.Controllers
 
         //Objeto que nos pertmite acceder a la BD mapaeada.
         private StudioLabDBEntities myDB;
+        private Usuarios usuBorrado;
 
 
         public ActionResult Listado()
@@ -30,5 +31,34 @@ namespace StudioLab.Controllers
 
             return View(vm);
         }
+
+        public ActionResult AltaUsuario()
+        {
+
+            return View();
+
+        }
+
+        public ActionResult BorradoUsuario(int id)
+        {
+
+            myDB = new StudioLabDBEntities();
+
+            //Recuperamos de la base de datos el usuario que queremos borrar
+            usuBorrado = myDB.Usuarios.Where(u => u.idUsuario == id).FirstOrDefault();
+
+            //Borramos de la base de datos
+            myDB.Usuarios.Remove(usuBorrado);
+
+            //Guardamos los cambios
+            myDB.SaveChanges();
+
+
+            return RedirectToAction("Listado");
+
+        }
+
+
+
     }
 }
