912e205bbba4af55e19e78b42e7d3d5138eda637
Aaron <aaronbellotrovira@gmail.com>
2017-03-02 19:16:36 +0100

    Más cambios.

--- StudioLab/StudioLab/Controllers/UsuariosController.cs
+++ StudioLab/StudioLab/Controllers/UsuariosController.cs
@@ -4,6 +4,7 @@ using StudioLab.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using System.Data.Entity;
 
 namespace StudioLab.Controllers
 {
@@ -15,7 +16,7 @@ namespace StudioLab.Controllers
         private StudioLabDBEntities myDB;
         private Usuarios usuBorrado;
         private Usuarios usuAlta;
-
+        private Usuarios usuEditar;
 
         public ActionResult Listado()
         {
@@ -92,6 +93,60 @@ namespace StudioLab.Controllers
 
         }
 
+        [HttpGet]
+        public ActionResult EditaUsuario(int id)
+        {
+
+            myDB = new StudioLabDBEntities();
+            usuEditar = new Usuarios();
+
+            EditaUsuarioViewModel usu = new EditaUsuarioViewModel();
+
+            //Recuperamos de la BD el usuario que queremos editar
+            usuEditar = myDB.Usuarios.Where(u => u.idUsuario == id).FirstOrDefault();
+
+            usu.Nombre = usuEditar.nombre;
+            usu.Apellido1 = usuEditar.apellido1;
+            usu.Apellido2 = usuEditar.apellido2;
+            usu.Edad = usuEditar.edad.ToString();
+            usu.Rol = usuEditar.usuarioRol.ToString();
+            usu.IdUsuario = id;
+
+            return View(usu);
+
+        }
+
+
+
+        [HttpPost]
+        public ActionResult EditaUsuario(EditaUsuarioViewModel model)
+        {
+
+            myDB = new StudioLabDBEntities();
+            usuEditar = new Usuarios();
+
+            //Esto comprueba si son correctos los datos del formulario
+            if (ModelState.IsValid)
+            {
+
+                usuEditar.nombre = model.Nombre;
+                usuEditar.apellido1 = model.Apellido1;
+                usuEditar.apellido2 = model.Apellido2;
+                usuEditar.edad = Int32.Parse(model.Edad);
+                usuEditar.usuarioRol = Int32.Parse(model.Rol);
+                usuEditar.idUsuario = model.IdUsuario;
+
+                myDB.Entry(usuEditar).State = EntityState.Modified;
+                myDB.SaveChanges();
+
+
+                return RedirectToAction("Listado");
+            }
+
+
+            return View();
+
+        }
 
 
     }
