234310a778296711aac51be041d50a904061fcb2
Aaron <aaronbellotrovira@gmail.com>
2017-03-03 01:22:48 +0100

    Añadidos estilos de Bootstrap a formulario de altas de usuario.

--- StudioLab/StudioLab/Models/ViewModels/AltaUsuarioViewModel.cs
+++ StudioLab/StudioLab/Models/ViewModels/AltaUsuarioViewModel.cs
@@ -10,18 +10,23 @@ namespace StudioLab.Models.ViewModels
     {
 
         [Required(ErrorMessage = "This field is Required")]
+        [Display(Name ="Nombre:")]
         public string Nombre { get; set; }
 
         [Required(ErrorMessage = "This field is Required")]
+        [Display(Name = "Apellido 1:")]
         public string Apellido1 { get; set; }
 
         [Required(ErrorMessage = "This field is Required")]
+        [Display(Name = "Apellido 2:")]
         public string Apellido2 { get; set; }
 
         [Required(ErrorMessage = "This field is Required")]
+        [Display(Name = "Edad:")]
         public string Edad { get; set; }
 
         [Required(ErrorMessage = "This field is Required")]
+        [Display(Name = "Rol:")]
         public string Rol { get; set; }
 
 
