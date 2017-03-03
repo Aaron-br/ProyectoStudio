57a8c08386a9961f10bc51939320005dfdf9acb7
Aaron <aaronbellotrovira@gmail.com>
2017-03-03 12:56:12 +0100

    Añadido script de paginacion al bundle

--- StudioLab/StudioLab/App_Start/BundleConfig.cs
+++ StudioLab/StudioLab/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace StudioLab
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js").Include("~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/jquery-{version}.js", "~/Scripts/jquery.bootpag.js").Include("~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
